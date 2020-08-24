97fa6c3c7efe99d306eed105cb3376f53896aecb
mekomlusa <none>
2020-08-24 10:10:03 -0500

    Added functions

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sys
 from os import walk
+import os
 import imghdr
 import csv
 import argparse
@@ -7,7 +8,7 @@ import argparse
 from flask import Flask, redirect, url_for, request
 from flask import render_template
 from flask import send_file
-
+import pandas as pd
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -20,23 +21,71 @@ def tagger():
     image = app.config["FILES"][app.config["HEAD"]]
     labels = app.config["LABELS"]
     not_end = not(app.config["HEAD"] == len(app.config["FILES"]) - 1)
-    print(not_end)
-    return render_template('tagger.html', not_end=not_end, directory=directory, image=image, labels=labels, head=app.config["HEAD"] + 1, len=len(app.config["FILES"]))
+    not_start = not (app.config["HEAD"] == 0)
+    prev_ind = app.config["PREV"]
+    return render_template('tagger.html', not_end=not_end, prev=prev_ind, not_start=not_start, directory=directory, image=image, labels=labels, head=app.config["HEAD"] + 1, len=len(app.config["FILES"]))
 
 @app.route('/next')
 def next():
-    image = app.config["FILES"][app.config["HEAD"]]
+    # image = app.config["FILES"][app.config["HEAD"]]
     app.config["HEAD"] = app.config["HEAD"] + 1
-    with open(app.config["OUT"],'a') as f:
+    app.config["PREV"] = False
+    # with open(app.config["OUT"],'a') as f:
+    #     for label in app.config["LABELS"]:
+    #         f.write(image + "," +
+    #         label["id"] + "," +
+    #         label["name"] + "," +
+    #         str(round(float(label["xMin"]))) + "," +
+    #         str(round(float(label["xMax"]))) + "," +
+    #         str(round(float(label["yMin"]))) + "," +
+    #         str(round(float(label["yMax"]))) + "\n")
+    app.config["LABELS"] = []
+    return redirect(url_for('tagger'))
+
+@app.route('/prev')
+def prev():
+    app.config["HEAD"] = app.config["HEAD"] - 1
+    app.config["PREV"] = True
+    # restore labels
+    image = app.config["FILES"][app.config["HEAD"]]
+    saved_output = pd.read_csv(app.config["OUT"])
+    past_labels = saved_output[saved_output['image'] == image]
+    if len(past_labels) > 0:
+        for index, row in past_labels.iterrows():
+            app.config["LABELS"].append({"id": str(row['id']), "name": str(row['name']), "xMin": str(row['xMin']),
+                                         "xMax": str(row['xMax']), "yMin": str(row['yMin']), "yMax": str(row['yMax'])})
+    return redirect(url_for('tagger'))
+
+# newly saved route
+@app.route('/savenew')
+def savenew():
+    image = app.config["FILES"][app.config["HEAD"]]
+    with open(app.config["OUT"], 'a') as f:
         for label in app.config["LABELS"]:
             f.write(image + "," +
-            label["id"] + "," +
-            label["name"] + "," +
-            str(round(float(label["xMin"]))) + "," +
-            str(round(float(label["xMax"]))) + "," +
-            str(round(float(label["yMin"]))) + "," +
-            str(round(float(label["yMax"]))) + "\n")
-    app.config["LABELS"] = []
+                    label["id"] + "," +
+                    label["name"] + "," +
+                    str(round(float(label["xMin"]))) + "," +
+                    str(round(float(label["xMax"]))) + "," +
+                    str(round(float(label["yMin"]))) + "," +
+                    str(round(float(label["yMax"]))) + "\n")
+    return redirect(url_for('tagger'))
+
+# modify labels route
+@app.route('/modify')
+def modify():
+    image = app.config["FILES"][app.config["HEAD"]]
+    saved_output = pd.read_csv(app.config["OUT"])
+    labels_without_this_image = saved_output[saved_output['image'] != image]
+    current_df_len = len(labels_without_this_image)
+    for i in range(len(app.config["LABELS"])):
+        labels_without_this_image.loc[current_df_len + i] = [image] + list(app.config["LABELS"][i].values())
+
+    if current_df_len > 0:
+        labels_without_this_image.to_csv(app.config["OUT"], index=False, header=False)
+    else:
+        labels_without_this_image.to_csv(app.config["OUT"], index=False)
+
     return redirect(url_for('tagger'))
 
 @app.route("/bye")
@@ -66,11 +115,6 @@ def label(id):
     app.config["LABELS"][int(id) - 1]["name"] = name
     return redirect(url_for('tagger'))
 
-# @app.route('/prev')
-# def prev():
-#     app.config["HEAD"] = app.config["HEAD"] - 1
-#     return redirect(url_for('tagger'))
-
 @app.route('/image/<f>')
 def images(f):
     images = app.config['IMAGES']
@@ -87,9 +131,13 @@ if __name__ == "__main__":
          directory += "/"
     app.config["IMAGES"] = directory
     app.config["LABELS"] = []
-    files = None
+    app.config["PREV"] = False
+    files = []
+    acceptable_file_extensions = ['.png', '.jpg', '.gif']
     for (dirpath, dirnames, filenames) in walk(app.config["IMAGES"]):
-        files = filenames
+        for f in filenames:
+            if os.path.splitext(f)[1] in acceptable_file_extensions:
+                files.append(f)
         break
     if files == None:
         print("No files")
